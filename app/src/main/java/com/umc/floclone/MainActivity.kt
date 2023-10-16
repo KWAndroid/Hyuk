3df5b2015a334825beea47a142f202d4759428e5
harry <harry7408@naver.com>
2023-10-17 00:15:10 +0900

    first commit

---
+++ app/src/main/java/com/umc/floclone/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.umc.floclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.umc.floclone.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+    }
+}
